dacf83e8c9658148120cb36c8b74b4bc2405210b
Hermann A. Kruse <47894798+hermannakruse@users.noreply.github.com>
2022-10-09 14:09:34 +0200

    Firsch von Visual Studio
    Signed-off-by: Hermann A. Kruse <47894798+hermannakruse@users.noreply.github.com>

---
+++ WebTestAzure/WebTestAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTestAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
