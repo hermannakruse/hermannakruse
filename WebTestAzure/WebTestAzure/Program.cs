05ac2e273501a4d8571aadc59710e249153aef88
Hermann A. Kruse <47894798+hermannakruse@users.noreply.github.com>
2022-10-14 09:00:39 +0200

    Neu Erstellt mit "PS C:\Users\hak.KRUSE-HIR\GIT\GitHub_hermannakruse\WebTestAzure\WebTestAzure> dotnet new razor" Git Diff ein wenig angepasst
    Signed-off-by: Hermann A. Kruse <47894798+hermannakruse@users.noreply.github.com>

--- WebTestAzure/WebTestAzure/Program.cs
+++ WebTestAzure/WebTestAzure/Program.cs
@@ -1,37 +1,25 @@
-namespace WebTestAzure
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-
-        public static bool showSec = false;
-        public static DateTime nowD = DateTime.Now;
-
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
